7589a82765ade2be7ed5df8b13a257b4e31ec036
Juris <juris.abolins.cs@gmail.com>
2021-11-21 13:04:27 +0200

    Class Transaction and class Partner

---
+++ src/main/java/accountingLog2021/domain/BasicProductGroup.java
@@ -0,0 +1,5 @@
+package accountingLog2021.domain;
+
+public enum BasicProductGroup {
+    MARKETING, FUEl, TRANSPORT, OFFICE_EQUIPMENT, HOUSEHOLD_GOODS, MATERIALS_FOR_BUSINESS
+}
---
+++ src/main/java/accountingLog2021/domain/Partner.java
@@ -0,0 +1,33 @@
+package accountingLog2021.domain;
+
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.*;
+
+@Entity(name = "Partner")
+@Table(name = "Partner")
+
+public class Partner {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column (name ="id")
+    private long id;
+
+    @Column (name= "PartnerFullName", nullable = false, length = 100)
+    private String partnerFullName;
+
+    @Column (name="PartnerShortName", nullable = false, length= 20)
+    private String partnerShortName;
+
+    @Enumerated (EnumType.STRING)
+    @Column (name="partnerBasicProduct")
+    private BasicProductGroup partnerBasicProductGroup;
+
+    @Enumerated (EnumType.STRING)
+    @Column (name = "attitudeForBusiness")
+    private AttitudeForBusiness attitudeForBusiness;
+
+    @Column (name = "partnerRegNr", length = 20)
+    private String partnerRegNr;
+
+}
